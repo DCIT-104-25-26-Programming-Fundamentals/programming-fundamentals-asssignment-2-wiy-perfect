c50d13a5c990059e78731c72ad836694cf4bfb2f
Lemuel Nkrumah <nlemuel190@gmail.com>
2026-08-01 08:33:41 +0000

    completed

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -40,5 +40,70 @@
 // =============================================================================
 
 #include <iostream>
-using namespace std;
+#include <vector>
 
+//functions
+int add(std::vector<int> numberList){
+    int sum= 0;
+    for (int i=0;i <= numberList.size()-1; i++ ){
+        sum += numberList[i];
+    }
+    return sum;
+}
+
+double average(std::vector<int> numberLList){
+   double mean= (double)add(numberList)/ numberList.size() //forcing double division since add() return an int
+   return mean
+}
+
+int max(std::vector<int> numberList){
+   int maxNum= numberList[0]
+   for (i=0; i <= numberList.size()-1; i++){
+    if (numberList[i]> maxNum){
+        maxNum = numberList[i]
+    }
+   }
+   return maxNum 
+}
+
+int min(std::vector <int> numberList){
+    int minNumber= numberList[0]
+    for (i=0; i<= numberList.size()-1; i++){
+        if (numberList[i] < minNumber){
+            minNumber= numberList[i]
+        }
+    }
+    return minNUmber
+}
+
+int main(){
+    int count;
+    std::cout<<"how many numbers? "<<std::endl;
+    std::cin>>count;
+
+    if (count<=0){
+        std::cout<<"Error: You entered an invalid number"<<std::endl;
+        return
+    }
+    //storing user input in a temporary variable and psuhing back to the 'numbers' array
+    std::vector<int> numbers= {};
+    for(i=0, i<= count-1;i++){
+        int temporaryNumber;
+        std::cout<<"Enter number"<<i+1<<" :"<<std::endl;
+        std::cin>> temporaryNumber;
+
+        numbers.push_back(temporaryNumber);
+        if (count[i]<=0){
+            std::cout<<"Error: The number you entered is less than 0"<<std::endl;
+            return 
+        }
+    }
+
+    //displaying the results
+    std::cout<<"Results: "<<std::endl;
+    std::cout<<"Sum:     "<<add(numbers)<<std::endl;
+    std::cout<<"Average: "<<average(numbers)<<std::endl;
+    std::cout<<"Maximum: "<<max(numbers)<<std::endl;
+    std::cout<<"Minimum: "<<min(numbers)<<std::endl;
+
+}
